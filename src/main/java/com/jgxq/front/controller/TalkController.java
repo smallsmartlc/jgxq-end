4387bb503525e770ed8cfb695eac73f0bd6af03d
smallsmartlc <1346683197@qq.com>
2021-01-09 15:57:02 +0800

    修改talk排序方式

--- src/main/java/com/jgxq/front/controller/TalkController.java
+++ src/main/java/com/jgxq/front/controller/TalkController.java
@@ -92,7 +92,7 @@ public class TalkController {
         Page<Talk> talkPage = new Page<>(pageNum, pageSize);
 
         QueryWrapper<Talk> talkQuery = new QueryWrapper<>();
-        talkQuery.orderByDesc("id");
+        talkQuery.orderByDesc("create_At");
 
         talkService.page(talkPage, talkQuery);
 
